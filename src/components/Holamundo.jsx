e2939bb16f6ed483269301d212e17ef8c02196d0
Roberto Luna <roberluna8@gmail.com>
2020-11-10 19:20:00 -0700

    first react commit

---
+++ src/components/Holamundo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Holamundo = () => {
+    const nombre  = 'roberto'
+    return (
+        <div>
+            <p>Hola mundo {nombre}</p>
+        </div>
+    )
+}
+
+export default Holamundo
---
+++ src/components/Parrafo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Parrafo = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quod laudantium accusantium. Repellat ipsa sapiente dignissimos, molestiae minus natus officiis tempora? Laborum aperiam deserunt consequatur ut aspernatur illo libero placeat!|</p>
+        </div>
+    )
+}
+
+export default Parrafo
