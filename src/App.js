6ac5060917702ae122559832ebc3d8e5f3e72250
Roberto Luna <roberluna8@gmail.com>
2020-11-12 10:42:50 -0700

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Parrafo from './components/Parrafo'
 import Holamundo from './components/Holamundo'
 import Contador from './components/Contador'
+import User from './components/User'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Parrafo></Parrafo>
       <Holamundo></Holamundo>
       <Contador></Contador>
+      <User></User>
     </div>
   );
 }
---
+++ src/components/User.jsx
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react'
+
+const User = () => {
+
+    const [users, setUsers] = useState([])
+
+    //created
+    useEffect(()=>
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(res =>  setUsers(res) )
+    )
+
+    return (
+        <div>
+            <ul>
+                {
+                        users.map(user => (
+                            <li key={user.id} >
+                                {user.name}
+                            </li>
+                        ))
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default User
